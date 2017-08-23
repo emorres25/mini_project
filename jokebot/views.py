f109ae9bbf29be8f5a9761ee7afbb0fd0fef98d8
Shaurya <Shaurya@emorres.local>
2017-08-23 17:10:40 +0530

    nm

--- jokebot/views.py
+++ jokebot/views.py
@@ -6,11 +6,11 @@ from django.http.response import HttpResponse
  
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
- 
+verify_token = '8510865767'
  
 def get_joke(fbid, recevied_message):
     joke_text = requests.get("http://api.icndb.com/jokes/random/").json()['value']['joke']
-    post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'%'Place your access token here'
+    post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'%'EAABkitQTatQBAPosC3d3O4d6dBRWC95mCuhHFfZCReaiqw3wZC1N36bktjjGyjyE6g1ve2ZBekHHo6hX6ZCOhHIwLZAd7lhQ9zaviSL6UVjp3eYu0un6swliknSPBuSAeFukSdQvcChdoiDEhCxhnJOuUmqRD39D7YJZAp1sDMfwZDZD'
     response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":joke_text}})
     status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
     pprint(status.json())
