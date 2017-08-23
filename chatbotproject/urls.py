d5ab43fbf4be2fe095cc3fae05beecf3759f89e3
Shaurya <Shaurya@emorres.local>
2017-08-23 17:33:38 +0530

    nm

--- chatbotproject/urls.py
+++ chatbotproject/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-
+from main_app.views import dictbot
 urlpatterns = patterns('',
-    
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^shaurya/?$', Botview.as_view()) 
+    # Examples:
+    # url(r'^$', 'dictbot.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
 
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^shaurya/?$', dictbot.as_view())
 )
--- jokebot/views.py
+++ jokebot/views.py
@@ -1,27 +1,32 @@
 import json, requests, random, re
 from pprint import pprint
- 
+import json
+from django.shortcuts import render
+from django.http import HttpResponse
+
 from django.views import generic
-from django.http.response import HttpResponse
- 
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 
-from django.shortcuts import render
-
+# Create your views here.
+access_token = 'EAABkitQTatQBAFe5xCcOjwfACiFvZCllhVZCaVXM48mMZCnVaay8iCXVdJCTLfZBTIAVhQCtaZBmk1ZCG1lqLB0JZALxOcPffzvoaZCh64RsZAzGs5rmweOuUsy4gtxcZCgr0HpJB1F39wF8wIFZBnEo7PXH1VIhf9pVxGLZCcPSo9564gZDZD'
 verify_token = '8510865767'
-VERIFY_TOKEN = verify_token
-access_token = 'EAABkitQTatQBAPosC3d3O4d6dBRWC95mCuhHFfZCReaiqw3wZC1N36bktjjGyjyE6g1ve2ZBekHHo6hX6ZCOhHIwLZAd7lhQ9zaviSL6UVjp3eYu0un6swliknSPBuSAeFukSdQvcChdoiDEhCxhnJOuUmqRD39D7YJZAp1sDMfwZDZD'
-ACCESS_TOKEN = access_token
- 
-def get_joke(fbid, recevied_message):
-    joke_text = requests.get("http://api.icndb.com/jokes/random/").json()['value']['joke']
-    post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'%'EAABkitQTatQBAPosC3d3O4d6dBRWC95mCuhHFfZCReaiqw3wZC1N36bktjjGyjyE6g1ve2ZBekHHo6hX6ZCOhHIwLZAd7lhQ9zaviSL6UVjp3eYu0un6swliknSPBuSAeFukSdQvcChdoiDEhCxhnJOuUmqRD39D7YJZAp1sDMfwZDZD'
-    response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":joke_text}})
+#url = 'http://api.wordnik.com:80/v4/word.json/tycoon/definitions?limit=200&includeRelated=true&useCanonical=false&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5'
+def get_meaning(fbid, recieved_message):
+    url = 'http://api.wordnik.com:80/v4/word.json/' + recieved_message.lower() + '/definitions?limit=200&includeRelated=true&useCanonical=false&includeTags=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5'
+    try:
+        r = requests.get(url).json()[0]["text"]
+        fdata = str(r)
+    except:
+        fdata = "The word was noy found!"
+
+    post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'% access_token
+    response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":fdata}})
     status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
     pprint(status.json())
- 
-class jokebot(generic.View):
+
+
+class dictbot(generic.View):
     def get(self, request, *args, **kwargs):
         if self.request.GET['hub.verify_token'] == verify_token:
             return HttpResponse(self.request.GET['hub.challenge'])
@@ -38,12 +43,11 @@ class jokebot(generic.View):
         for entry in incoming_message['entry']:
             for message in entry['messaging']: 
                 if 'message' in message: 
-                    get_joke(message['sender']['id'], message['message']['text'])    
+                    try:  
+                        get_meaning(message['sender']['id'], message['message']['text'])
+                        #send_yo()
+                    except Exception as e:
+                        print e
+                        get_meaning(message['sender']['id'], 'Please send a valid text.')    
+                        #send_yo()
         return HttpResponse()
-
-class BotView(generic.View):
-    def get(self, request, *args, **kwargs):
-        if self.request.GET['hub.verify_token'] == VERIFY_TOKEN:
-            return HttpResponse(self.request.GET['hub.challenge'])
-        else:
-            return HttpResponse('Error, invalid token')
